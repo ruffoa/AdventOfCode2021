492dbd119e0e7967146ea91efe29d2ae88449bef
alexanderr <alexander_ruffo@trendmicro.com>
2021-12-03 14:38:47 -0500

    Day 3 - part 2

--- day 3/code.js
+++ day 3/code.js
@@ -1,6 +1,7 @@
 import { readFileSync } from "fs";
 import { dirname } from "path"
 import { fileURLToPath } from 'url';
+import chalk from "chalk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -46,6 +47,8 @@ function part1() {
 
   console.log(`Result: `, epsilon * gamma )
 
+  console.log('\n\n------------------------------------------------------\n');
+
   part2(vals);
   // console.log(distance);
 
@@ -63,66 +66,84 @@ function part2(ogVals) {
       return "1";
     return "0";
   });
-  let co2NVals = o2NVals;
+  let co2NVals = JSON.parse(JSON.stringify(o2NVals));
+  console.log('starting', o2NVals);
 
-  let oxygenVals = new Set(lines);
-  let co2Vals = new Set(lines);
+  let oxygenVals = deepClone(lines);
+  let co2Vals = deepClone(lines);
+
+  let o2Match = o2NVals[0];
+  let coMatch = 1 - parseInt(o2Match);
 
   const nChars = input.split('\n')[0].split("").length;
 
   for (let i = 0; i < nChars; i++) {
-    for (const line of input.split('\n')) {
-      const charArr = line.split("");
+    for (let p = 0; p < oxygenVals.length; p++) {
+      const charArr = oxygenVals[p].split("");
       
-      if (charArr[i] != o2NVals[i]) {
-        if (oxygenVals.size > 1) {
-          oxygenVals.delete(line);
+      if (charArr[i] != o2Match) {
+        if (oxygenVals.length > 1) {
+          oxygenVals[p] = null;
         }
-      } 
-      if (charArr[i] == co2NVals[i] && co2Vals.size > 1) {
-          co2Vals.delete(line);
+      }
+      // console.log(oxygenVals[p], charArr, o2NVals);
+    }
+
+    // let numRemaining = co2Vals.length;
+    for (let p = 0; p < co2Vals.length; p++) {
+      // console.log(co2Vals[p], co2NVals, p, co2Vals);
+      const charArr = co2Vals[p].split("");
+      
+      if (charArr[i] != coMatch) {
+        if (co2Vals.length > 1) {
+          // numRemaining--;
+          co2Vals[p] = null;
+        } else {
+          console.log("We are done with CO2, there is only 1 val left!", co2Vals);
+        }
       }
       // console.log(line, charArr, vals);
     }
 
+    oxygenVals = oxygenVals.filter((val) => val);
+    co2Vals = co2Vals.filter((val) => val);
+
+    let o2Zeroes = 0;
+    let co2Zeroes = 0;
+
+    let o2Ones = 0;
+    let co2Ones = 0;
+
     // determine most common bits
-    for (let i = 0; i < nChars; i++) {
-      o2NVals[i] = 0;
-      co2NVals[i] = 0;
-    }
-  
-    for (const val of Array.from(oxygenVals)) {
+    for (const val of oxygenVals) {
       const charArr = val.split("");
 
-      for (let i = 0; i < charArr.length; i++) {
-        o2NVals[i] += parseInt(charArr[i]);
-      } 
+      if (charArr[i+1] == 0) {
+        o2Zeroes ++;
+      } else {
+        o2Ones ++;
+      }
     }
 
-    for (const val of Array.from(co2Vals)) {
+    for (const val of co2Vals) {
       const charArr = val.split("");
 
-      for (let i = 0; i < charArr.length; i++) {
-        co2NVals[i] += parseInt(charArr[i]);
-      } 
+      if (charArr[i+1] == 0) {
+        co2Zeroes ++;
+      } else {
+        co2Ones ++;
+      }
     }
 
-    console.log(`DEBUG`, o2NVals, co2NVals, oxygenVals, co2Vals, oxygenVals.size, co2Vals.size)
-    o2NVals = o2NVals.map((val) => { 
-      const avg = val / oxygenVals.size;
-      if (avg >= 0.5) 
-        return "1";
-      return "0";
-    });
+    o2Match = o2Ones >= o2Zeroes ? 1 : 0;
+    coMatch = co2Ones >= co2Zeroes ? 0 : 1;
 
-    co2NVals = co2NVals.map((val) => { 
-      const avg = val / co2Vals.size;
-      if (avg > 0.5) 
-        return "1";
-      return "0";
-    });
-  
-    console.log(`o2`, oxygenVals, `co2`, co2Vals, i, `o2 avg: `, o2NVals[i], `co2 avg`, co2NVals[i]);
+    console.log(`o2`, oxygenVals.length, `co2`, co2Vals.length, 'next char #', i + 1, `o2 next: `, o2Match, `co2 next`, coMatch, `o2 vals`, oxygenVals.map((val) => {
+      return val.split("").map((ch, n) => n == (i+1) ? ` ${ch} ` : ch).join('')
+    }),
+    `co2 vals`, co2Vals.map((val) => {
+      return val.split("").map((ch, n) => n == (i+1) ? ` ${ch} ` : ch).join('')
+    }));
   }
 
   console.log("O2 Vals")
@@ -137,4 +158,9 @@ function part2(ogVals) {
   console.log(`o2`, o2, `co2`, c02, `res`, c02 * o2);
 }
 
+function deepClone(input) {
+  return JSON.parse(JSON.stringify(input));
+}
+
 part1();
+
